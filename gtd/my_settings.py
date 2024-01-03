e57f6a3558fa8ce502a48ebefc56ebe83b246916
kimsunjae <woal9844@gmail.com>
2024-01-04 03:26:54 +0900

    chore : 초기 환경 세팅

---
+++ gtd/my_settings.py
@@ -0,0 +1,13 @@
+# MYDATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'gtddb',
+#         'USER' : 'sunjae',
+#         'PASSWORD': 'sunjae!!!',
+#         'HOST' : 'gtddb.cboug6ag2ziw.ap-northeast-2.rds.amazonaws.com',
+#         'PORT' : '3306',
+#         'OPTIONS' : {
+#             'init_command' : 'SET sql_mode="STRICT_TRANS_TABLES"'
+#         }
+#     }
+# }
