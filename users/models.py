76bcf0b6fe9fcc3cd1d7c93bb59e003b3aa80f62
SNOW <kancth03@gmail.com>
2024-01-08 04:45:43 +0900

    feat: Docs 모델 연결을 위해 User Model 생성

--- users/models.py
+++ users/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password, **kwargs):
+        """
+        주어진 이메일, 비밀번호 등 개인정보로 User 인스턴스 생성
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=email,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+# AbstractBaseUser를 상속해서 유저 커스텀
+class User(AbstractBaseUser, PermissionsMixin):
+    id = models.AutoField(primary_key=True)  # pk
+    email = models.EmailField(default='', max_length=255, null=False, unique=True)
+    password = models.CharField(max_length=255, null=False)
+    nickname = models.CharField(max_length=255, default='', null=False, unique=True)
+
+    created_at = models.DateTimeField(auto_now_add=True, null=False)
+    updated_at = models.DateTimeField(auto_now=True, null=False)
+    is_deleted = models.BooleanField(default=False, null=False)
+
+    # 헬퍼 클래스 사용
+    objects = UserManager()
+
+    # 사용자의 username field는 email으로 설정 (이메일로 로그인)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
