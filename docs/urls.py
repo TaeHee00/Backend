8623d6f8ab647c5356fe1ec7e0b0a4b07f38087e
김동현 <kdh@MacBookPro.local>
2024-01-08 23:29:53 +0900

    feat: 문서 상세 조회

--- docs/urls.py
+++ docs/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns =[
     path('docs/', views.DocsList.as_view()), # get - 문서 내역 조회
+    path('docs/<int:pk>/', views.DocsDetail.as_view()), # get - 문서 상세 내역 조회
 ]
--- docs/views.py
+++ docs/views.py
@@ -5,8 +5,19 @@ from rest_framework.views import APIView
 from .models import Docs
 from .serializers import Serializer
 class DocsList(APIView):
-    def get(self, request): # 문서 내역 목록 조회
+    def get(self, request): # 문서 조회
         docs = Docs.objects.filter(is_deleted=False)  # is_deleted가 False인 객체만 조회
         serializer = Serializer(docs, many=True)
+        return Response(serializer.data)
+
+class DocsDetail(APIView): # Docs의 detail을 보여주는 역할
+    def get_object(self, pk):  # Docs 객체 가져오기
+        try:
+            return Docs.objects.get(pk=pk, is_deleted=False)  # is_deleted가 False인 객체만 조회
+        except Docs.DoesNotExist:
+            raise Http404
+    def get(self, request, pk): # 문서 상세 조회
+        doc = self.get_object(pk)
+        serializer = Serializer(doc)
         return Response(serializer.data)
 
